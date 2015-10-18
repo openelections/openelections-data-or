45ee7630b136dc71380e966a0945075fc94ec692
Derek Willis <dwillis@gmail.com>
2015-10-17 20:48:15 -0400

    working version for 2014 primary

--- josephine_parser.py
+++ josephine_parser.py
@@ -2,43 +2,85 @@ import unicodecsv
 from BeautifulSoup import BeautifulSoup
 headers = ['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes']
 parties = ['(DEMOCRAT)', '(REPUBLICAN)', '(NONPARTISAN)']
-offices = ['United States Senator', 'Representative in Congress', 'State Representative', 'Governor']
+offices = ['United States Senator', 'Representative in Congress, 2nd District', 'Representative in Congress, 4th District', 'State Representative, 1st District', 'State Representative, 2nd District', 'State Representative, 3rd District', 'State Representative, 4th District', 'Governor']
+office_lookup = {
+    'United States Senator' : 'U.S. Senate', 'Representative in Congress' : 'U.S. House', 'Governor' : 'Governor', 'State Senator' : 'State Senate',
+    'State Representative' : 'State House', 'Secretary of State' : 'Secretary of State', 'Attorney General' : 'Attorney General',
+    'State Treasurer' : 'State Treasurer'
+}
 
 with open('20140520__or__primary__josephine__precinct.csv', 'wb') as csvfile:
     w = unicodecsv.writer(csvfile, encoding='utf-8')
     w.writerow(headers)
 
-    file = open("/Users/DW-Admin/code/openelections-sources-or/Josephine/May14.htm").read()
+    file = open("/Users/derekwillis/code/openelections-sources-or/Josephine/May14.htm").read()
     soup = BeautifulSoup(file)
     lines = soup.find('pre').text.split('\r\n')
+    keys = []
     for line in lines:
         if line.strip() == '\n':
             continue
         if "NUMBERED KEY CANVASS" in line:
             continue
-        if lines[1][0:10].strip() == '':
+        if line.strip() == '':
             continue
+#        if lines[1][0:10].strip() == '':
+#            continue
         if 'RUN DATE:' in line:
+            continue
+        if 'STATISTICS' in line:
             continue
         if '- - -' in line:
             continue
-        if "REGISTERED VOTERS" or "BALLOTS CAST" or "VOTER TURNOUT" in line:
+        if '-----' in line:
+            continue
+        if '==' in line:
+            continue
+        if "REGISTERED VOTERS" in line:
+            continue
+        if "BALLOTS CAST" in line:
+            continue
+        if "VOTER TURNOUT" in line:
+            continue
+        if 'PERCENT' in line:
+            continue
+        if 'ELECTION' in line:
+            continue
+        if 'of the' in line:
             continue
         if line.strip() == 'Vote For  1':
             continue
-        # parse offices
-        if "=" in line:
-            # get candidate keys
-            candidate_bits = [x.strip() for x in line.split('   ') if '=' in x]
-            candidates = [x.split(' = ') for x in candidate_bits]
-            for code, name in candidates:
-                keys.append({'code': code, 'name': name})
         if line.strip().split("    ")[0:3] == [u'01', u'02', u'03']:
             continue
+
+        if any(party in line for party in parties):
+            party = line.replace("(",'').replace(")",'').strip()
+            continue
+        # parse offices, reset keys
+        if any(office in line for office in offices):
+            if "District" in line.strip():
+                o, d = line.strip().split(', ')
+                office = office_lookup[o.strip()]
+                district = d[0]
+            else:
+                office = office_lookup[line.strip()]
+                district = None
+            keys = []
+            continue
+        if "=" in line:
+            if office:
+                # get candidate keys
+                candidate_bits = [x.strip() for x in line.split('   ') if '=' in x]
+                candidates = [x.split(' = ') for x in candidate_bits]
+                for code, name in candidates:
+                    keys.append({'code': int(code), 'name': name})
+                continue
         # once we have all keys, sort them in order
         candidate_keys = sorted(keys, key=lambda k: k['code'])
-        print candidate_keys
         # parse vote data, should match keys
         result_bits = [x for x in line.split(' ') if x <> '']
-        result_bits = result_bits[1:] # remove dupe precinct
-        w.writerow(result_bits)
+        precinct = result_bits[0]
+        result_bits = result_bits[1:] # remove precinct info
+        if len(result_bits) > 0:
+            for cand in candidate_keys:
+                w.writerow(['Josephine', precinct, office, district, party, cand['name'], result_bits[cand['code']]])
