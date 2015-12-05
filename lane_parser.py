d77500281d122627475fe376cadc07dc38b9e596
Derek Willis <dwillis@gmail.com>
2015-12-05 09:27:26 -0500

    updated Lane parser for primaries

--- lane_parser.py
+++ lane_parser.py
@@ -3,24 +3,25 @@ import requests
 import unicodecsv
 from BeautifulSoup import BeautifulSoup
 headers = ['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes']
-parties = ['DEMOCRAT', 'REPUBLICAN']
+parties = ['(Democratic)', '(Republican)']
 party_abbrevs = ['(REP)', '(DEM)', '(GRN)', '(PRO)', '(LIB)', '(CON)', '(PAC)', '(LBT)', '(WFP)', '(PGP)', '(PCE)', '(IND)']
-offices = ['United States President', 'US Senator', 'US Representative, 4th District', 'Representative in Congress, 4th District', 'State Representative, 7th District', 'State Representative, 8th District',
-'State Representative, 9th District', 'State Representative, 11th District', 'Governor', 'State Representative, 12th District', 'State Representative, 13th District', 'State Representative, 14th District',
-'State Treasurer', 'Attorney General', 'Secretary of State', 'State Senator, 1st District', 'State Senator, 2nd District', 'Representative, Congress, 4th District', 'United States President and VP',
-'Representative, Congress 4th Dist. 4TH DISTRICT', 'Representative, Congress 2nd Dist. 2ND DISTRICT', 'State Senator, 4th District', 'State Senator, 6th District', 'State Senator, 7th District', ]
+offices = ['United States President', 'United States Senator', 'US Representative, 4th District', 'Representative in Congress, 4th District', 'State Representative 7th District', 'State Representative 8th District',
+'State Representative 9th District', 'State Representative 11th District', 'Governor', 'State Representative 12th District', 'State Representative 13th District', 'State Representative 14th District',
+'State Treasurer', 'Attorney General', 'Secretary of State', 'State Senator, 1st District', 'State Senator, 2nd District', 'Representative, Congress, 4th District', 'United States President and Vice President',
+'Representative in Congress 4th District', 'State Senator, 5th District', 'State Senator 4th District', 'State Senator 6th District', 'State Senator 7th District', ]
 office_lookup = {
-    'US Senator' : 'U.S. Senate', 'US Representative' : 'U.S. House', 'Governor' : 'Governor', 'State Senator' : 'State Senate',
+    'United States Senator' : 'U.S. Senate', 'Representative in Congress' : 'U.S. House', 'Governor' : 'Governor', 'State Senator' : 'State Senate',
     'State Representative' : 'State House', 'Secretary of State' : 'Secretary of State', 'Attorney General' : 'Attorney General',
     'State Treasurer' : 'State Treasurer', 'Representative' : 'U.S. House', 'United States President' : 'President', 'Representative, Congress' : 'U.S. House',
-    'United States President and VP' : 'President', 'Representative, Congress 4th Dist': 'U.S. House', 'Representative, Congress 2nd Dist': 'U.S. House'
+    'United States President and Vice President' : 'President', 'Representative in Congress 4th District': 'U.S. House', 'Representative, Congress 2nd Dist': 'U.S. House'
 }
 
-with open('20141104__or__general__lane__precinct.csv', 'wb') as csvfile:
+with open('20140520__or__primary__lane__precinct.csv', 'wb') as csvfile:
     w = unicodecsv.writer(csvfile, encoding='utf-8')
     w.writerow(headers)
 
-    r = requests.get("http://www.lanecounty.org/Departments/CAO/Operations/CountyClerk/elections/ElectionDocuments/20141104_FinalCanvas.HTM")
+    r = requests.get("http://www.lanecounty.org/Departments/CAO/Operations/CountyClerk/elections/ElectionDocuments/20140520_FinalCanvas.HTM")
+#    lines = r.text.split('\r\n')
     soup = BeautifulSoup(r.text)
     lines = soup.find('pre').text.split('\r\n')
     keys = []
@@ -49,6 +50,8 @@ with open('20141104__or__general__lane__precinct.csv', 'wb') as csvfile:
             continue
         if "VOTER TURNOUT" in line:
             continue
+        if 'VOTERS' in line:
+            continue
         if 'PERCENT' in line:
             continue
         if 'Election' in line:
@@ -61,16 +64,21 @@ with open('20141104__or__general__lane__precinct.csv', 'wb') as csvfile:
             continue
         # for primary
         if any(party in line for party in parties):
-            party = line.replace("(",'').replace(")",'').strip()
+            party = line.replace("(",'').replace(")",'').strip()[0:3].upper()
             continue
         # parse offices, reset keys
         if any(office in line for office in offices):
             print line.strip()
             if "DISTRICT" in line.strip().upper():
-                if 'State' in line.strip():
-                    o, d = line.strip().split(', ')
-                elif 'Dist.' in line.strip():
-                    o, d = line.strip().split('. ')
+                if 'State Senator' in line.strip():
+                    o = 'State Senator'
+                    d = line.strip().split('State Senator ')[1]
+                elif 'State Representative' in line.strip():
+                    o = 'State Representative'
+                    d = line.strip().split('State Representative ')[1]
+                elif line.strip() == 'Representative in Congress 4th District':
+                    o = 'Representative in Congress'
+                    d = '4th District'
                 else:
                     o, d = line.strip().split(', ')
                 office = office_lookup[o.strip()]
@@ -86,14 +94,14 @@ with open('20141104__or__general__lane__precinct.csv', 'wb') as csvfile:
                 candidate_bits = [x.strip() for x in line.split('   ') if '=' in x]
                 candidates = [x.split(' = ') for x in candidate_bits]
                 for code, name in candidates:
-                    if any(party in line for party in party_abbrevs):
-                        party = next(party for party in party_abbrevs if party in line)
-                        name = name.replace(party, '').strip()
-                        party = party.replace('(','').replace(')','')
-                    else:
-                        party = None
-                    if name in ['WRITE-IN', 'OVER VOTES', 'UNDER VOTES']:
-                        party = None
+#                    if any(party in line for party in party_abbrevs):
+#                        party = next(party for party in party_abbrevs if party in line)
+#                        name = name.replace(party, '').strip()
+#                        party = party.replace('(','').replace(')','')
+#                    else:
+#                        party = None
+#                    if name in ['WRITE-IN', 'OVER VOTES', 'UNDER VOTES']:
+#                        party = None
                     keys.append({'code': int(code), 'name': name, 'party': party})
                 continue
         # once we have all keys, sort them in order
