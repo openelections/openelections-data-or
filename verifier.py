6beb009ac57746e0f78af52e93a66ce1fd12da16
Nick Kocharhook <nickocha@microsoft.com>
2016-11-27 22:18:58 +0000

    Differentiated between general and primary files. Added a bunch of tests

--- verifier.py
+++ verifier.py
@@ -26,12 +26,13 @@ import csv
 import os
 import re
 import argparse
+from difflib import SequenceMatcher
 
 def main():
 	args = parseArguments()
 
 	for path in args.paths:
-		verifier = OEVerifier(path)
+		verifier = Verifier(path)
 
 		if verifier.ready and "matrix" not in verifier.filename:
 			verifier.verify()
@@ -40,16 +41,39 @@ def main():
 def parseArguments():
 	parser = argparse.ArgumentParser(description='Verify openelections CSV files')
 	parser.add_argument('paths', metavar='N', type=str, nargs='+',
-	                   help='path to a CSV file')
+					   help='path to a CSV file')
 
 	return parser.parse_args()
 
 
-class OEVerifier(object):
+class Verifier(object):
 	validColumns = frozenset(['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes', 'notes'])
 	requiredColumns = frozenset(['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes'])
 	validOffices = frozenset(['President', 'U.S. Senate', 'U.S. House', 'Governor', 'State Senate', 'State House', 'Attorney General', 'Secretary of State', 'State Treasurer'])
 	officesWithDistricts = frozenset(['U.S. House', 'State Senate', 'State House'])
+	pseudocandidates = frozenset(['Write-in', 'Under Votes', 'Over Votes', 'Total'])
+	normalizedPseudocandidates = frozenset(['writein', 'undervotes', 'overvotes', 'total'])
+
+	# Return the appropriate subclass based on the path
+	def __new__(cls, path):
+		if cls is Verifier:
+			filename = os.path.basename(path)
+
+			if "general" in filename:
+				if "precinct" in filename:
+					return super(Verifier, cls).__new__(GeneralPrecinctVerifier)
+				else:
+					return super(Verifier, cls).__new__(GeneralVerifier)
+			elif "primary" in filename:
+				if "precinct" in filename:
+					return super(Verifier, cls).__new__(PrimaryPrecinctVerifier)
+				else:
+					return super(Verifier, cls).__new__(PrimaryVerifier)
+			elif "special" in filename and "precinct" in filename:
+				return super(Verifier, cls).__new__(SpecialPrecinctVerifier)
+
+		else:
+			return super(Verifier, cls).__new__(cls, path)
 
 	def __init__(self, path):
 		self.path = path
@@ -64,13 +88,12 @@ class OEVerifier(object):
 
 			self.filename = os.path.basename(path)
 			self.filenameState, self.filenameCounty = self.deriveStateCountyFromFilename(self.filename)
+
+			self.ready = True
 		except Exception as e:
 			print("ERROR: {}".format(e))
 
-		self.ready = True
-
 	def verify(self):
-		filetype = self.deriveTypeFromPath(self.path)
 		self.parseFileAtPath(self.path)
 
 	def pathSanityCheck(self, path):
@@ -81,20 +104,6 @@ class OEVerifier(object):
 			raise ValueError("Filename does not end in .csv: %s" % path)
 
 		print("==> {}".format(path))
-
-	def deriveTypeFromPath(self, path):
-		if "general" in self.filename:
-			if "precinct" in self.filename:
-				return OEFileType.GeneralPrecinct
-			else:
-				return OEFileType.General
-		elif "primary" in self.filename:
-			if "precinct" in self.filename:
-				return OEFileType.PrimaryPrecinct
-			else:
-				return OEFileType.Primary
-		elif "special" in self.filename and "precinct" in self.filename:
-			return OEFileType.SpecialPrecinct
 
 	def deriveStateCountyFromFilename(self, filename):
 		components = filename.split("__")
@@ -110,13 +119,15 @@ class OEVerifier(object):
 			
 			if self.verifyColumns(self.reader.fieldnames):
 				for row in self.reader:
-					self.verifyCounty(row['county'], row)
-					self.verifyOffice(row['office'], row)
+					self.verifyCounty(row)
+					self.verifyOffice(row)
 					self.verifyDistrict(row)
+					self.verifyCandidate(row)
+					self.verifyParty(row)
 
 	def verifyColumns(self, columns):
-		invalidColumns = set(columns) - OEVerifier.validColumns
-		missingColumns = OEVerifier.requiredColumns - set(columns)
+		invalidColumns = set(columns) - Verifier.validColumns
+		missingColumns = Verifier.requiredColumns - set(columns)
 
 		if invalidColumns:
 			self.printError("Invalid columns: {}".format(invalidColumns))
@@ -127,30 +138,52 @@ class OEVerifier(object):
 
 		return True
 
-	def verifyCounty(self, county, row):
-		normalisedCounty = county.title()
+	def verifyCounty(self, row):
+		normalisedCounty = row['county'].title()
 
 		if not normalisedCounty == self.filenameCounty:
 			self.printError("County doesn't match filename", row)
 
-		if not county == normalisedCounty:
+		if not row['county'] == normalisedCounty:
 			self.printError("Use title case for the county", row)
 
-	def verifyOffice(self, office, row):
-		if not office in OEVerifier.validOffices:
-			self.self.printError("Invalid office: {}".format(office), row)
+	def verifyOffice(self, row):
+		if not row['office'] in Verifier.validOffices:
+			self.self.printError("Invalid office: {}".format(row['office']), row)
 
 	def verifyDistrict(self, row):
-		if row['office'] in OEVerifier.officesWithDistricts:
+		if row['office'] in Verifier.officesWithDistricts:
 			if not row['district']:
 				self.printError("Office '{}' requires a district".format(row['office']), row)
 			elif row['district'].lower() == 'x':
 				if self.filenameState == 'ms':
-					pass # This is legit in MS
+					pass # This is legit in some MS precincts
 				else:
 					self.printError("District must be an integer", row)
 			elif not self.verifyInteger(row['district']):
 				self.printError("District must be an integer", row)
+
+	def verifyCandidate(self, row):
+		charsRE = re.compile('[^A-Za-z]+', re.UNICODE)
+		candidate = row['candidate']
+		normalizedCandidate = charsRE.sub('', candidate).lower()
+
+		if candidate not in Verifier.pseudocandidates:
+			if normalizedCandidate in Verifier.normalizedPseudocandidates:
+				self.printError("Misspelled pseudocandidate: '{}'".format(candidate), row)
+			else:
+				# Compare the normalized strings to determine if they match
+				for npc in Verifier.normalizedPseudocandidates:
+					s = SequenceMatcher(None, normalizedCandidate, npc)
+					match = s.find_longest_match(0, len(normalizedCandidate), 0, len(npc))
+
+					if match.size > 4:
+						self.printError("Misspelled pseudocandidate: '{}'".format(candidate), row)
+						break
+
+	def verifyParty(self, row):
+		if row['candidate'] not in Verifier.pseudocandidates and not row['party']:
+			self.printError("Party missing", row)
 
 	def verifyInteger(self, numberStr):
 		try:
@@ -164,12 +197,27 @@ class OEVerifier(object):
 		print("ERROR: " + text)
 
 		if row:
-			print row
+			print(row)
 
 
+class GeneralPrecinctVerifier(Verifier):
+	pass
 
-class OEFileType(object):
-	General, Primary, GeneralPrecinct, PrimaryPrecinct, SpecialPrecinct = range(1,6)
+class PrimaryPrecinctVerifier(Verifier):
+	def verifyParty(self, row):
+		if not row['party']:
+			self.printError("Primary results must include a party for every row", row)
+
+class SpecialPrecinctVerifier(Verifier):
+	pass
+
+
+class PrimaryVerifier(Verifier):
+	pass
+
+class GeneralVerifier(Verifier):
+	pass
+
 
 # Default function is main()
 if __name__ == '__main__':
