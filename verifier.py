b647f4e558a12c50904d0d25519870efde1b8bb5
Nick Kocharhook <nickocha@microsoft.com>
2016-11-28 11:25:31 +0000

    Update verifier to use "Write-ins" not "Write-In"

--- verifier.py
+++ verifier.py
@@ -51,8 +51,8 @@ class Verifier(object):
 	requiredColumns = frozenset(['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes'])
 	validOffices = frozenset(['President', 'U.S. Senate', 'U.S. House', 'Governor', 'State Senate', 'State House', 'Attorney General', 'Secretary of State', 'State Treasurer'])
 	officesWithDistricts = frozenset(['U.S. House', 'State Senate', 'State House'])
-	pseudocandidates = frozenset(['Write-in', 'Under Votes', 'Over Votes', 'Total'])
-	normalizedPseudocandidates = frozenset(['writein', 'undervotes', 'overvotes', 'total'])
+	pseudocandidates = frozenset(['Write-ins', 'Under Votes', 'Over Votes', 'Total'])
+	normalizedPseudocandidates = frozenset(['writeins', 'undervotes', 'overvotes', 'total'])
 
 	# Return the appropriate subclass based on the path
 	def __new__(cls, path):
